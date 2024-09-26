faed7109bf065845d744a25ff6dc469eb0fa4b45
Denez F-K <denez.flegeau-kihal@outlook.com>
2024-09-26 18:28:11 +0200

    Update Class GPS
    Gestion de la fonction __str__

--- codeMain/lib/GPSdata.py
+++ codeMain/lib/GPSdata.py
@@ -2,16 +2,25 @@ import machine
 import time
 from micropyGPS import MicropyGPS  # https://github.com/inmcm/micropyGPS
 
+def convert_to_dms(coords):
+
+        degrees, decimal_minutes, direction = coords
+        # Séparer la partie entière des minutes de la partie décimale
+        minutes = int(decimal_minutes)
+        # Convertir la partie décimale des minutes en secondes
+        seconds = (decimal_minutes - minutes) * 60
+        # Retourner le format en DMS avec 1 décimale pour les secondes
+        return f"{degrees}deg{minutes}'{seconds:.1f}\"{direction}"
 
 class GPSdata:
-    def __init__(self,uartPin,baudRate=9600,tx=4, rx=5, timeout=5000, timeout_char=5000) -> None:
+    def __init__(self,uartPin,baudRate=9600,tx=8, rx=9, timeout=5000, timeout_char=5000) -> None:
             self.uart= machine.UART(uartPin,baudrate=baudRate,tx=machine.Pin(tx), rx=machine.Pin(rx), timeout=timeout, timeout_char=timeout_char)  # initialisation UART
             print(self.uart)
             self.gps = MicropyGPS() # création d'un objet GPS
     
     def update(self)->int:
         if self.uart.any(): 
-            donnees_brutes = str(self.uart.readline())
+            donnees_brutes = str(self.uart.readline())  #fonction bloquante
             for x in donnees_brutes:
                 self.gps.update(x)
 
@@ -26,9 +35,11 @@ class GPSdata:
 
     def __str__(self) -> str:
         self.update()
-        return str(self.gps.latitude_string())+";"+str(self.gps.longitude_string())+";"+str(self.gps.altitude())+";"+str(self.gps.speed_string('kph'))+";"+str(self.gps.date_string('s_dmy'))
+        #return str(self.gps.latitude_string())+";"+str(self.gps.longitude_string())+";"+str(self.gps.altitude())+";"+str(self.gps.speed_string('kph'))+";"+str(self.gps.date_string('s_dmy'))
         # return str("00")+";"+str("00")+";"+str("00")+";"+str("00")+";"+str("00")
-    
+        return str(convert_to_dms(self.gps.latitude) + ";" + convert_to_dms(self.gps.longitude) + ";" +  self.gps.altitude() + ";" + self.gps.satellites_visible() + ";" + self.gps.speed_string('km/h') + ";" +  self.gps.gprmc())
+        #latitude;longitude;altitude;nombre satellite;vitesse;heure
+
 if __name__ == "__main__":
     gp = GPSdata(1,rx=9,tx=8)
     while True:
