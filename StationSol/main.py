1c4c4b8da93bec29ac882fd7bd1a40cbe4861295
Pierre-Melo <149083913+Pierre-Melo@users.noreply.github.com>
2024-10-01 13:08:28 +0200

    Updating Station Sol

--- StationSol/main.py
+++ StationSol/main.py
@@ -1,15 +1,12 @@
 from lib.sx1262 import SX1262
-import time
-import threading
-import uselect
 import sys
 
 #Connections :
 #SX   |     PIN -> GPIO
-#MOD  |	MODULE 1 | MODULE 2	
-#BUS  | BUS 1	 | BUS 0	
-#CLK  | 38 -> 26 | 3  -> 2	
-#MOSI | 39 -> 27 | 5  -> 3	
+#MOD  |    MODULE 1 | MODULE 2    
+#BUS  | BUS 1     | BUS 0    
+#CLK  | 38 -> 26 | 3  -> 2    
+#MOSI | 39 -> 27 | 5  -> 3    
 #MISO | 40 -> 28 | 6  -> 4
 #CS   | 30 -> 19 | 14 -> 11
 #DIO1 | 31 -> 20 | 15 -> 12
@@ -27,11 +24,10 @@ def cbRX(events): #callback function when RX event occurs
         error = SX1262.STATUS[err]
         print(msg)
         print(error)
+    print(sxRX.getRSSI())
 def cbTX(events): #callback function when TX event occurs
     if events & SX1262.TX_DONE:
         print('TX done.')
-def read1():
-    return(sys.stdin.read(1) if spoll.poll(0) else None)
 
 sxRX.begin(freqRX, bw=500.0, sf=12, cr=8, syncWord=0x34,
          implicit=False, implicitLen=0xFF,
@@ -45,20 +41,3 @@ sxTX.begin(freqTX, bw=500.0, sf=12, cr=8, syncWord=0x34,
 
 sxRX.setBlockingCallback(False, cbRX)
 sxTX.setBlockingCallback(False, cbTX)
-
-spoll=uselect.poll()
-spoll.register(sys.stdin,uselect.POLLIN)
-
-send_data = ""
-send_char = "#" #char used to end line and send data
-
-# main loop
-while True:
-    c = read1()
-    if c != None:
-        if c == send_char:
-            sxTX.send(send_data)
-            send_data = ""
-            pass
-        else:
-            send_data = send_data + c
